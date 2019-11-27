c90ffc64738d665dda9ecef8d4aaec86e0f0d041
vitor-apolinario <vitorapoli@gmail.com>
2019-11-26 22:26:30 -0300

    header feito na raça

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Quiz from 'react-quiz-component';
 import { quiz } from './quiz';
+import Header from './components/header/header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Quiz quiz={quiz}/>
     </div>
   );
