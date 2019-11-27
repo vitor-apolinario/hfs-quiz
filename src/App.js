942f2aaab6946e26ad8606d22b95c6bd67cfe0bd
WillianGenero <willian-b-g@hotmail.com>
2019-11-26 23:35:09 -0300

    Rodapé contendo os devs e link para github

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Quiz from 'react-quiz-component';
 import { quiz } from './quiz';
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Quiz quiz={quiz}/>
+      <Footer />
     </div>
   );
 }
